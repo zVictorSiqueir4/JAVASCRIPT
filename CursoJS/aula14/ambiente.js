daf4d2941f801b4b6b4668ceff30f3acfd6be5b8
Victor <victorsoliver20@outlook.com>
2022-04-04 22:29:17 -0300

    aula 14 js

--- CursoJS/aula14/ambiente.js
+++ CursoJS/aula14/ambiente.js
@@ -1,2 +1,20 @@
+/*
 var c = 1
-kcjsvbglkjmrae
+while (c <= 5){
+    console.log(c)
+    c++
+}*/
+
+//--------------------
+/*
+do{
+    console.log(c)
+    c++
+}while(c <= 5)
+*/
+
+console.log('Vai começar...')
+for(var c = 1; c <= 4; c++){
+    console.log(c)
+}
+console.log('FIM!')
